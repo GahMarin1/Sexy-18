ca3cad6888a9848dbb9bcc75e0ed88920085c7c6
GahMarin1 <jaque1805.r@gmail.com>
2024-10-26 22:24:59 -0300

    Update Sexy +18.lua

--- Sexy +18.lua
+++ Sexy +18.lua
@@ -11,28 +11,6 @@ local window = PabloLib:Create(
 --Tabs
 local tab1 = window:CreateTab("Main")
 
---Key System
-local PabloLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/BatuKvi123/PabloLibV3/main/PabloLibV3"))()
-local key = "PabloLibKey" -- Put your key here.
-local window = PabloLib:Create(
-"Key System",
-"Enabled"
-"p"
-)
-
-local tab1 = window:CreateTab("Key System")
-tab1:CreateWarning("Please put key to contiune.")
-tab1:CreateTextbox("Put key here:", function(a)
-if a == key then
--- Put your code here what happens after you entered the correct key.
-window:CreateTab("TestTab")
-else
-local warning = tab1:CreateWarning("Wrong key! Please try again.") -- If the key is wrong it will creates a warning says "Wrong Key! Please try again." after 2 seconds it deletes. 
-wait(2)
-warning:Delete() 
-end
-end)
-
 --Buttons
 tab1:CreateButton("Infinity Yield", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
